5228c78053e490edba0966454a73573e9175a554
Ben Greaves <bsgreaves@gmail.com>
2017-03-06 19:57:28 -0600

    Started xhr

--- main.js
+++ main.js
@@ -1,1 +1,14 @@
-console.log('test');
+function execute (){
+	console.log("It worked")
+};
+
+function fail (){
+	console.log("It failed")
+};
+
+var myRequest = new XMLHttpRequest();
+myRequest.addEventListener("load", execute);
+myRequest.addEventListener("error", fail);
+myRequest.open("GET", "dinosaurs.json");
+myRequest.send();
+console.log("myRequest", myRequest);
