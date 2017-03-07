11098a1e5ace4c8ac029d6685b3e529efcffa968
Ben Greaves <bsgreaves@gmail.com>
2017-03-06 21:17:52 -0600

    Done

--- main.js
+++ main.js
@@ -1,9 +1,34 @@
-function execute (){
-	console.log("It worked")
+console.log("First:", Date.now());
+var outputDiv = document.getElementById("dinoContainer");
+
+
+function makeDOM(xhrData) {
+	var dinosaurString = "";
+	console.log("buildingDom:", Date.now());
+	for (let i = 0; i < xhrData.dinosaurs.length; i++) {
+		currDino = xhrData.dinosaurs[i];
+		dinosaurString += `
+			<div class="col-sm-6 col-md-4">
+    			<div class="thumbnail">
+      				<img src="${currDino.url}" alt="...">
+      				<div class="caption">
+        				<h3>${currDino.name}</h3>
+        				<p>The ${currDino.type} who eats ${currDino.food}</p>
+      				</div>
+   				</div>
+  			</div>`;
+	}
+	outputDiv.innerHTML = dinosaurString;
+}
+
+function execute() {
+	console.log("Executing:", Date.now());
+    var data = JSON.parse(this.responseText);
+    makeDOM(data);
 };
 
-function fail (){
-	console.log("It failed")
+function fail() {
+    console.log("It failed");
 };
 
 var myRequest = new XMLHttpRequest();
@@ -11,4 +36,5 @@ myRequest.addEventListener("load", execute);
 myRequest.addEventListener("error", fail);
 myRequest.open("GET", "dinosaurs.json");
 myRequest.send();
-console.log("myRequest", myRequest);
+
+console.log("Last:", Date.now());
